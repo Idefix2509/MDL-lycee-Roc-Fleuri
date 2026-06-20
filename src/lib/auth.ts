377ac9a3b183feefdc392ad3541b6a56cb804bb0
Idefix2509 <sarlat.2509@gmail.com>
2026-06-20 16:44:47 +0200

    Create getCurrentUserId function for user simulation
    Add a temporary function to simulate getting the current user ID.

---
+++ src/lib/auth.ts
@@ -0,0 +1,13 @@
+export async function getCurrentUserId() {
+
+  /*
+   TEMPORAIRE
+
+   Ici nous brancherons Clerk plus tard.
+
+   Pour l'instant on simule l'utilisateur connecté.
+  */
+
+  return 1;
+
+}
